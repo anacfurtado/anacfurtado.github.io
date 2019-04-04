7be234f1a36a01c7d9c92177b6efe5dc490cf8a7
Ana Furtado <furta735@newschool.edu>
2019-04-04 09:17:54 -0400

    sdf

---
+++ lab/exercise7/query.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+
+	/* $('.all').on('click',function(){
+
+			$('all').addClass('.newbg');
+			
+		});
+
+	$('.sun').on('mouseenter',function(){
+
+			$('.sun').addClass('newbg');
+			
+		}); */
+
+	$('.sun').on('click',function(){
+
+			$('.sun').addClass('animation');
+			
+		});
+
+	$('.sun').on('dblclick',function(){
+
+			$('.sun').addClass('newbg');
+			
+		});
+
+	
+
+});
