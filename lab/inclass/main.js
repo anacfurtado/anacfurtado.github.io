75f699c4a6ef850f9a7efdcc8dad72f6426d6223
Ana Furtado <furta735@newschool.edu>
2019-04-05 10:18:38 -0400

    sfv

---
+++ lab/inclass/main.js
@@ -0,0 +1,61 @@
+alert ("Welcome! Please view console on your browser.")
+
+
+
+var name = ["Ana Clara Marques Furtado"]
+var integer = [7]
+var number = ["7"]
+var egg = true;
+
+
+console.log(name)
+console.log(integer)
+console.log(number)
+console.log(egg)
+
+console.log (5+2)
+console.log (6+1)
+console.log (10-3)
+console.log (3+4)
+console.log (14/2)
+
+
+
+
+var countries = ["South Africa", "Australia", "Japan", "Greece"]
+console.log (countries[0])
+console.log (countries.length)
+console.log( "The current time is " + new Date ())
+
+
+
+
+var currentHour = new Date().getHours()
+console.log (currentHour)
+
+
+	if (currentHour = 11) {
+		var msg = "It is 11 now"
+	} else {
+		var msg = "It is not 11 yet"
+	} 
+
+console.log(msg)
+
+
+if (name.length - 2 <= 9)  {
+  var msg = "My name is relatively short."
+} else if (name.length - 2 >= 14) {
+  var msg = "My name is relatively long."
+} else {
+  var msg = "My name is neither short nor long."
+}
+
+console.log(msg);
+
+
+
+
+
+
+
